a6429287e1f4e27dc70f651a13c7aedfdba7de7e
Tùng.PT <jeniferchoe2712@gmail.com>
2022-06-26 20:28:08 +0700

    update

--- views/Banner/create.php
+++ views/Banner/create.php
@@ -1,1 +1,74 @@
-<h1>view hien thi trang them banner</h1>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Thêm mới Banner
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li><a href="#">Forms</a></li>
+            <li class="active">General Elements</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <!-- left column -->
+            <div class="col-md-6">
+                <!-- general form elements -->
+                <div class="box box-primary">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Thêm banner</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <!-- form start -->
+                    <form role="form">
+                        <div class="box-body">
+                            <div class="form-group">
+                                <label for="exampleInputTilte">Tiêu đề</label>
+                                <input required id="title" name="tilte" type="text" class="form-control"  placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputImage">Chọn ảnh</label>
+                                <input id="image" name="image" type="file" id="image">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputUrl">Liên kết</label>
+                                <input type="text" class="form-control" id="url" name="url" placeholder="">
+                            </div>
+
+                            <div class="form-group">
+                                <label>Chọn Target</label>
+                                <select class="form-control">
+                                    <option value="_blank">blank</option>
+                                    <option value="_self"></option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="exampleInputFile">File input</label>
+                                <input type="file" id="exampleInputFile">
+
+                                <p class="help-block">Example block-level help text here.</p>
+                            </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox"> Check me out
+                                </label>
+                            </div>
+                        </div>
+                        <!-- /.box-body -->
+
+                        <div class="box-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.box -->
+            </div>
+            <!--/.col (left) -->
+            <!-- /.row -->
+    </section>
+    <!-- /.content -->
+</div>
